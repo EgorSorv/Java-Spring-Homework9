ab2dbdc14b8f6cd5880b915c57a59dbf89385e86
ЕГОР <egor@MacBook.local>
2024-05-08 16:29:31 +0300

    initial git

---
+++ Note_service/src/test/java/ru/gb/Note_service/NoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.Note_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
